6adac56d86b3a4a37756bd9e1bd0cecfd437bd56
Jesse Roe <jessedroe@gmail.com>
2016-03-16 01:04:34 -0400

    added place_token function and various test in driver

--- c4_driver.py
+++ c4_driver.py
@@ -2,8 +2,20 @@ from c4_engine import *
 
 g1 = Game(6, 8, 3)
 g1.board[0][0] = 1
+g1.board[0][1] = 1
 g1.board[1][1] = 1
 g1.board[2][1] = 1
+# g1.board[3][1] = 1
+# g1.board[4][1] = 1
+# g1.board[5][1] = 1
+
+print g1.place_token(1, 1)
+print g1.place_token(1, 1)
+print g1.place_token(1, 1)
+print g1.place_token(1, 2)
+
+print g1.place_token(1, 7)
+
 
 
 print type(g1.board)
@@ -11,6 +23,6 @@ print type(g1.board)
 g1.print_formated()
 
 print g1.check_full_board()
-
+print g1.check_for_col_height(1)
 
 # g1.print_formated()
--- c4_engine.py
+++ c4_engine.py
@@ -1,19 +1,18 @@
 
-# from prettytable import PrettyTable
 
 class Game:	
+
 	board = 0
 	height = 0
 	width = 0
 	row_len = 0
+
 	def __init__ ( self, height, width, row_len ):
 		Game.height = height
 		Game.width = width
-		Game.row_len = row_len		
-
+		Game.row_len = row_len	
 		b = lambda h, w: [[-1] * w for i in range(h)]		
 		Game.board = b(height, width)
-
 
 	def print_formated (self):
 		# x = lambda b: for row in b: print row		
@@ -38,7 +37,17 @@ class Game:
 		return True		
 					
 	def check_for_col_height (self, col):
-		for row in reversed(range(self.height)):
-			if (board[row][col] == -1):
+		for row in range(self.height):
+			if (self.board[row][col] == -1):
 				return row
 		return -1
+
+	def place_token (self, player, col):
+		if (col > (self.width - 1) or col < 0 or player > 1 or player < 0):
+			return False
+		open_row = self.check_for_col_height(col)
+		if (open_row == -1):
+			return False
+		else:
+			self.board[open_row][col] = player
+			return True
